7a2e78616220253be8937d14a9721c2e382ad945
Chris Jones <chris@jonezy.org>
2012-04-17 19:41:20 -0400

    updated converter plugin to exclude most of the fancy pants stuff

--- _plugins/interview_generator.rb
+++ _plugins/interview_generator.rb
@@ -10,31 +10,6 @@ module Jekyll
 		end
 		
 		def convert(content)
-			wares = Dir.glob('./{hardware,software}/*.yml')
-			slugs = []
-			
-			content.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/).each do |link|
-				slugs << (link[1] ? link[1] : link[0].downcase)
-			end
-			
-			if slugs.length > 0
-				content += "\n\n"
-				
-				wares.each do |ware|
-					base_name = File.basename(ware, ".yml")
-				
-					if slugs.include? base_name
-						begin
-							data = YAML.load_file(ware)
-							content += "[#{base_name}]: " + data["url"] + " \"" + data["description"] + "\"\n"
-						
-							slugs.delete(base_name)
-						rescue
-						end
-					end
-				end
-			end
-			
 			Maruku.new(content).to_html
 		end
 	end
